24d5b44b8b7e217bcc398a05b1a853b538bba7ae
koki.hagiwara <hagiwara_k12@gmail.com>
2021-06-01 15:45:07 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
       @tasks = Task.all
   end
@@ -43,11 +44,14 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-private
-
   
-# Strong Parameter
-  def task_params
+  private
+
+　  def set_task
+     @task = Task.find(params[:id])
+    end
+
+    def task_params
     params.require(:task).permit(:content)
-  end
+    end
 end
